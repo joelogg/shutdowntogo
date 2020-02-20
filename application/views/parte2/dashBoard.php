29b3452fcb52a3cfd3d700f2e2f2118105e71dbe
joelogg <joel.o.gallegos.g@gmail.com>
2020-02-20 13:12:35 -0500

    Dashboard1

--- application/views/parte2/dashBoard.php
+++ application/views/parte2/dashBoard.php
@@ -1,35 +1,48 @@
 <div class="row px-3">
-        <div class="col-md-12 col-lg-6 col-xl-4 divGrafDashBoard">
-            <span class="titGraficasDashBoard">% COMPLETADO</span>
-            <div class="divUnGrafDashBoard">
 
-                <div id="gauge_g1" style="height: 250px;"></div>
+    <div class="col-12 col-lg-8 col-xl-9">
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-12 col-xl-6 divGrafDashBoard">
+                <span class="titGraficasDashBoard">% COMPLETADO</span>
+                <div class="divUnGrafDashBoard">
+
+                    <div id="gauge_g1" style="height: 250px;"></div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-12 col-xl-6 divGrafDashBoard">
+                <span class="titGraficasDashBoard">ESTATUS</span>
+                <div class="divUnGrafDashBoard">
+                    
+                    <div id="donut_g2" style="height: 250px;"></div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-12 col-xl-6 divGrafDashBoard">
+                <span class="titGraficasDashBoard">PRIORIDAD</span>
+                <div class="divUnGrafDashBoard">
+
+                    <div id="bar_g3" style="height: 250px;"></div>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-6 col-lg-12 col-xl-6 divGrafDashBoard">
+                <span class="titGraficasDashBoard">ÁREAS</span>
+                <div class="divUnGrafDashBoard">
+
+                    <div id="bar_g4" style="height: 250px;"></div>
+                </div>
             </div>
         </div>
+    </div>
 
-        <div class="col-md-12 col-lg-6 col-xl-4 divGrafDashBoard">
-            <span class="titGraficasDashBoard">ESTATUS</span>
-            <div class="divUnGrafDashBoard">
-                
-                <div id="donut_g2" style="height: 250px;"></div>
-            </div>
-        </div>
-
-        <div class="col-md-12 col-lg-6 col-xl-4 divGrafDashBoard">
-            <span class="titGraficasDashBoard">PRIORIDAD</span>
-            <div class="divUnGrafDashBoard">
-
-                <div id="bar_g3" style="height: 250px;"></div>
-            </div>
-        </div>
-
-        <div class="col-md-12 col-lg-6 col-xl-4 divGrafDashBoard">
-            <span class="titGraficasDashBoard">ÁREAS</span>
-            <div class="divUnGrafDashBoard">
-
-                <div id="bar_g4" style="height: 250px;"></div>
-            </div>
-        </div>
-
+    <div class="col-12 col-lg-4 col-xl-3" style="background-color: white; ">
+        Hola
+    </div>
 
 </div>
+
+
+        
+        
+        
--- desarrollo/js/parte2/dashboard.js
+++ desarrollo/js/parte2/dashboard.js
@@ -33,6 +33,9 @@ function mostrarGraficaDashBoard()
                 [ 'Reprogramadas', 7 ]
             ],
             type : 'donut',
+            onclick: function (d, i) { console.log("onclick", d, i); },
+            //onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+            //onmouseout: function (d, i) { console.log("onmouseout", d, i); }
         },
         donut: { title: '35 OT' },
     });
@@ -41,47 +44,96 @@ function mostrarGraficaDashBoard()
     c3.generate(
     {
         bindto: '#bar_g3',
-        color: { pattern: [ '#FF5722', '#4CAF50' ] },
         data: 
         {
+            x : 'x',
             columns: 
             [
-                [ 'Abiertas', 118, 124, 332, 262, 182 ],
-                [ 'En progreso', 118, 124, 332, 262, 182 ],
-                [ 'Abiertas', 118, 124, 332, 262, 182 ],
-                [ 'Finalizadas', 118, 124, 332, 262, 182 ],
-                [ 'Reprogramadas', 138, 164, 474, 244, 216 ]
+                ['x', 'Muy alta', 'Alta', 'Media', 'Baja'],
+                [ 'Abiertas', 118, 124, 332, 262 ],
+                [ 'En progreso', 118, 124, 332, 262 ],
+                [ 'Finalizadas', 118, 124, 332, 262 ],
+                [ 'Atrazadas', 118, 124, 332, 262 ],
+                [ 'Reprogramadas', 138, 164, 474, 244 ]
             ],
             type: 'bar',
             groups: 
             [
-                ['data1','data2']
+                ['Abiertas','En progreso', 'Finalizadas', 'Atrazadas', 'Reprogramadas']
             ],
         },
         bar: 
         {
             width: { ratio: 0.5 },
         },
+        axis: 
+        {
+            rotated: true,
+            x: 
+            {
+                type: 'category',
+                tick: 
+                {
+                    multiline: true
+                }
+            },
+        }
     });
 
     c3.generate(
-    {
-        bindto: '#bar_g4',
-        color: { pattern: [ '#FF5722', '#4CAF50' ] },
-        data: 
         {
-            columns: 
-            [
-                [ 'data1', 492, 118, 124, 332, 262, 182 ],
-                [ 'data2', 205, 138, 164, 474, 244, 216 ]
-            ],
-            type: 'bar',
-        },
-        bar: 
-        {
-            width: { ratio: 0.5 },
-        },
-    });
+            bindto: '#bar_g4',
+            data: 
+            {
+                x : 'x',
+                columns: 
+                [
+                    ['x', 'Area1', 'Area2', 'Area3', 'Area4'],
+                    [ 'Abiertas', 118, 124, 332, 262 ],
+                    [ 'En progreso', 118, 124, 332, 262 ],
+                    [ 'Finalizadas', 118, 124, 332, 262 ],
+                    [ 'Atrazadas', 118, 124, 332, 262 ],
+                    [ 'Reprogramadas', 138, 164, 474, 244 ]
+                ],
+                type: 'bar',
+                groups: 
+                [
+                    ['Abiertas','En progreso', 'Finalizadas', 'Atrazadas', 'Reprogramadas']
+                ],
+            },
+            bar: 
+            {
+                width: { ratio: 0.5 },
+            },
+            axis: 
+            {
+                x: 
+                {
+                    type: 'category',
+                    tick: 
+                    {
+                        multiline: true
+                    }
+                },
+            }
+        });
 
     
 }
+
+
+
+/*
+chart.load({
+        columns: [
+            ["setosa", 0.2, 0.2, 0.2, 0.2, 0.2, 0.4, 0.3, 0.2, 0.2, 0.1, 0.2, 0.2, 0.1, 0.1, 0.2, 0.4, 0.4, 0.3, 0.3, 0.3, 0.2, 0.4, 0.2, 0.5, 0.2, 0.2, 0.4, 0.2, 0.2, 0.2, 0.2, 0.4, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.2, 0.2, 0.3, 0.3, 0.2, 0.6, 0.4, 0.3, 0.2, 0.2, 0.2, 0.2],
+            ["versicolor", 1.4, 1.5, 1.5, 1.3, 1.5, 1.3, 1.6, 1.0, 1.3, 1.4, 1.0, 1.5, 1.0, 1.4, 1.3, 1.4, 1.5, 1.0, 1.5, 1.1, 1.8, 1.3, 1.5, 1.2, 1.3, 1.4, 1.4, 1.7, 1.5, 1.0, 1.1, 1.0, 1.2, 1.6, 1.5, 1.6, 1.5, 1.3, 1.3, 1.3, 1.2, 1.4, 1.2, 1.0, 1.3, 1.2, 1.3, 1.3, 1.1, 1.3],
+            ["virginica", 2.5, 1.9, 2.1, 1.8, 2.2, 2.1, 1.7, 1.8, 1.8, 2.5, 2.0, 1.9, 2.1, 2.0, 2.4, 2.3, 1.8, 2.2, 2.3, 1.5, 2.3, 2.0, 2.0, 1.8, 2.1, 1.8, 1.8, 1.8, 2.1, 1.6, 1.9, 2.0, 2.2, 1.5, 1.4, 2.3, 2.4, 1.8, 1.8, 2.1, 2.4, 2.3, 1.9, 2.3, 2.5, 2.3, 1.9, 2.0, 2.3, 1.8],
+        ]
+    });
+
+
+chart.unload({
+        ids: 'data1'
+    });
+*/
